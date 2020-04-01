6c65434427384acc2d0bb9b350520be3d0efbe48
Kate1900 <61184952+Kate1900@users.noreply.github.com>
2020-04-01 13:32:46 +0300

    Mammals

---
+++ Mammals.h
@@ -0,0 +1,22 @@
+#ifndef _Mammals_h_
+#define _Mammals_h_
+
+#include "Animals.h"
+class TMammals : public TAnimals
+{
+protected:
+	int pol;
+	bool sherst;
+	bool pozvonochnik;
+public:
+	TMammals();
+	int GetPol();
+	bool GetSherst();
+	bool GetPozvonochnik();
+
+	void SetSherst(bool s);
+
+	double Moloco();
+	const char* Golos(); 
+};
+#endif
